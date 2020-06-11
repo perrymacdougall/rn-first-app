f73a03cebfde284c1acd52d7045e712a7d69e7c8
perrymacdougall <perrymacdougall@gmail.com>
2020-06-11 16:38:11 -0400

    setup stylesheet

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course goal" style={styles.input} />
+        <Button title="ADD" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    padding: 10
+  }
+})
