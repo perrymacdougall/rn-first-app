18e02c930b40a8f20790155899945c80d877a7e9
perrymacdougall <perrymacdougall@gmail.com>
2020-06-11 21:29:33 -0400

    set up button to change state thru hooks

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course goal" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput 
+          placeholder="Course goal"
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value={enteredGoal} 
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
     </View>
   );
