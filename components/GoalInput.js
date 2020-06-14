bcb2cc7129553862bf5dfb9d9a2b088725c85f8a
perrymacdougall <perrymacdougall@gmail.com>
2020-06-14 02:10:46 -0400

    set modal to close on item entry and added basic styles

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -9,7 +9,7 @@ const GoalInput = props => {
     };
     
     return (
-        <Modal visible={props.visible}>
+        <Modal visible={props.visible} animationType="slide">
             <View style={styles.inputContainer}>
                 <TextInput 
                 placeholder="Course goal"
@@ -25,15 +25,16 @@ const GoalInput = props => {
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     },
     input: {
         width: '80%',
         borderBottomWidth: 1,
         borderBottomColor: 'black',
-        padding: 10
+        padding: 10,
+        marginBottom: 10
     },
 })
 
